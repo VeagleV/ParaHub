0d03b2ca70fd4e84d3ffc9a32a8503e26914dd45
VeagleV <lanselot4267@gmail.com>
2025-10-08 00:29:25 +0300

    new hope

---
+++ src/main/java/org/bin/parahub/ParaHubApplication.java
@@ -0,0 +1,13 @@
+package org.bin.parahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bin/parahub/ParaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bin.parahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
